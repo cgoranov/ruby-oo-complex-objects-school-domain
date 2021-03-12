4d80de5031050bf723878c30765e5e8983bd03f0
Che <cgoranov@gmail.com>
2021-03-12 15:19:02 -0600

    commit tree f3adc43f1ba21893781099591a132910eb6773d1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+require 'pry'
+
+class School
+
+    attr_accessor :school_name
+    attr_reader :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster.include?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster_sort = {}
+        @roster.each do |k, v|
+            @roster_sort[k] = v.sort 
+        end
+        @roster_sort.sort.to_h
+    end
+
+end
